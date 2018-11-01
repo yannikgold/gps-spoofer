306dc3fbcc2f8b3d463eb4468ade9a8b4f476253
Yannik Goldgräbe <ygold@Yanniks-MacBook-Pro.local>
2018-11-01 15:08:11 +0100

    Added basic location functionality

--- app/src/main/java/hsfl/androidsec/de/gps_spoofer/MapsActivity.java
+++ app/src/main/java/hsfl/androidsec/de/gps_spoofer/MapsActivity.java
@@ -1,18 +1,28 @@
 package hsfl.androidsec.de.gps_spoofer;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager lm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
     }
 
 
@@ -34,13 +45,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Flensburg and move the camera
-        LatLng flensburg = new LatLng(54.792636, 9.431104);
+        /*LatLng flensburg = new LatLng(54.792636, 9.431104);
         mMap.addMarker(new MarkerOptions().position(flensburg).title("Marker in Flensburg"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(flensburg));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(flensburg));*/
+
+        Toast.makeText(getApplicationContext(), "Map loaded", Toast.LENGTH_SHORT).show();
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500,0, mLocationListener);
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
+            Marker marker = mMap.addMarker(new MarkerOptions().position(newLocation));
+            Toast.makeText(getApplicationContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 }
